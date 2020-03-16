1ecbe604a413e6640ad31f5d32c7adb98e28ff89
Aleš Bizjak <ab@concordium.com>
2020-03-16 20:43:55 +0100

    Command line arguments.

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Proxy
 import Yesod
+import qualified Network.Wai.Handler.Warp
 
 import System.Exit(die)
 import Control.Monad.Except
 
 import Concordium.Client.GRPC
+import Concordium.Client.Commands
+import Options.Applicative
+
+data DBOptions = DBOptions {
+  dbConnString :: String
+  }
+
+dbOptions :: Parser DBOptions
+dbOptions = do
+  let dbString = strOption (long "db" <> short 'd' <> metavar "CONNSTRING")
+  DBOptions <$> dbString
+
+parser :: ParserInfo (Backend, DBOptions)
+parser = info (helper <*> ((,) <$> backendParser <*> dbOptions))
+         (fullDesc <> progDesc "Generate transactions for a fixed contract.")
+
 
 defaultConfig :: GrpcConfig
 defaultConfig = GrpcConfig "localhost" 11109 Nothing
 
-dbConnString :: String
-dbConnString = "host=localhost port=5432 user=concordium dbname=concordium password=concordium"
+-- dbConnString :: String
+-- dbConnString = "host=localhost port=5432 user=concordium dbname=concordium password=concordium"
+
+runSite :: YesodDispatch site => Int -> Network.Wai.Handler.Warp.HostPreference -> site -> IO ()
+runSite port host site = do
+
+    toWaiApp site >>= Network.Wai.Handler.Warp.runSettings (
+        Network.Wai.Handler.Warp.setPort port $
+        Network.Wai.Handler.Warp.setServerName "Concordium-wallet-proxy" $
+        Network.Wai.Handler.Warp.setHost host $
+        Network.Wai.Handler.Warp.defaultSettings)
 
 main :: IO ()
-main =
+main = do
+  (backend, dbOptions) <- execParser parser
   runExceptT (mkGrpcClient defaultConfig) >>= \case
     Left err -> die $ "Cannot connect to GRPC endpoint: " ++ show err
-    Right cfg -> warp 3000 (Proxy cfg)
+    Right cfg ->
+      runSite 3000 "0.0.0.0" (Proxy cfg)
