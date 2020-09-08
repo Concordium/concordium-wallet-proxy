d854c036be025fc974d626e86f5ef58f24ab6ba5
Javier Sagredo <js@concordium.com>
2020-09-08 17:37:19 +0200

    Add reject reason for invalid index on encrypted transfers

--- src/Internationalization/En.hs
+++ src/Internationalization/En.hs
@@ -43,6 +43,7 @@ translation = I18n {..}
         i18nRejectReason InvalidEncryptedAmountTransferProof = "The encrypted amount transfer has an invalid proof."
         i18nRejectReason (EncryptedAmountSelfTransfer _) = "Encrypted amount transfer from account A to account A is not allowed."
         i18nRejectReason InvalidTransferToPublicProof  = "The secret to public transfer has an invalid proof."
+        i18nRejectReason InvalidIndexOnEncryptedTransfer = "The provided index is below the start index or above `startIndex + length incomingAmounts`."
 
         i18nTransactionType TTDeployModule = "Deploy module"
         i18nTransactionType TTInitContract = "Initialize smart contract"
