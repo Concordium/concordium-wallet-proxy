a50a36acf47ba472dfad60daad9a24b26dec08bc
Michael Bisgaard Olesen <mo@concordium.com>
2021-08-11 10:07:17 +0200

    Split k8s entrypoint from default one

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,25 +2,15 @@
 
 set -euxo pipefail
 
-reporters="${WALLET_PROXY_REPORTERS}"
-index="${WALLET_PROXY_REPORTER_INDEX}"
-
-drop_account="${WALLET_PROXY_ACCOUNT_FILE-}"
-ip_data="${WALLET_SERVER_INFOS_FILE}"
-
-# Read reporter IP and DB credentials from env based on the provided index.
-reporter_args_tsv="$(jq -r ".[${index}] | [.ip, .grpc.port, .db.port, .db.user, .db.name, .db.password] | @tsv" <<< "${reporters}")"
-IFS=$'\t' read -r ip grpc_port db_port db_user db_name db_password <<< "${reporter_args_tsv}"
-
 args=(
-	--grpc-ip "${ip}"
-	--grpc-port "${grpc_port}"
-	--db "host=${ip} port=${db_port} user=${db_user} dbname=${db_name} password=${db_password}"
-	--ip-data "${ip_data}"
+	--grpc-ip "${GRPC_HOST}"
+	--grpc-port "${GRPC_PORT}"
+	--ip-data "${IP_DATA_FILE}"
+	--db "host=${DB_HOST} port=${DB_PORT} user=${DB_USER} dbname=${DB_NAME} password=${DB_PASSWORD}"
 )
-if [ -n "${drop_account}" ]; then
-	args+=( --drop-account "${drop_account}" )
+if [ -n "${DROP_ACCOUNT_FILE-}" ]; then
+	args+=( --drop-account "${DROP_ACCOUNT_FILE}" )
 fi
 
 # Inherits env vars and args.
-/wallet-proxy "${args[@]}" "$@"
+/wallet-proxy "${args[@]}" "${@}"
---
+++ scripts/docker-entrypoint_k8s-statefulset.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# Get list of active reporter nodes and connection info from env.
+reporters="${REPORTER_LIST_JSON}"
+pod_name="${K8S_POD_NAME}"
+
+# Extract index from pod name (format: "<name>-<index>")
+# by stripping the longest prefix matching the pattern "*-" from 'pod_name'.
+index="${pod_name##*-}"
+
+# Parse reporter IP and DB credentials based on the pod index (using round-robin).
+# JSON format: "[{grpc: {host, port}, db: {host, name, port, user, password}}]".
+reporter_args_tsv="$(jq -r ".[${index} % (.|length)] | [.grpc.host, .grpc.port, db.host, .db.port, .db.user, .db.name, .db.password] | @tsv" <<< "${reporters}")"
+IFS=$'\t' read -r grpc_host grpc_port db_host db_port db_user db_name db_password <<< "${reporter_args_tsv}"
+
+# Invoke default entrypoint - inherits env vars and args.
+GRPC_HOST="${grpc_host}" \
+GRPC_PORT="${grpc_port}" \
+DB_HOST="${db_host}" \
+DB_PORT="${db_port}" \
+DB_USER="${db_user}" \
+DB_NAME="${db_name}" \
+DB_PASSWORD="${db_password}" \
+/docker-entrypoint.sh "${@}"
