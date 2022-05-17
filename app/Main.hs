fbceee81a9677318a187bf645409af33def64942
Aleš Bizjak <ab@concordium.com>
2022-05-17 21:25:27 +0200

    Add a forced update endpoint.

--- app/Main.hs
+++ app/Main.hs
@@ -23,12 +23,14 @@ import Control.Monad.Logger
 import Concordium.Client.GRPC
 import Concordium.Client.Commands as CMDS
 import Options.Applicative
+import Data.Range.Parser
 
 data ProxyConfig = ProxyConfig {
   pcGRPC :: GrpcConfig,
   pcDBConnString :: ByteString,
   -- | Account file used for GTU drop. Only used on stagenet and testnet.
   pcGTUAccountFile :: Maybe FilePath,
+  pcForcedUpdateConfigFile :: Maybe FilePath,
   pcHealthTolerance :: Maybe Int,
   pcIpInfo :: FilePath
 }
@@ -41,6 +43,7 @@ parser = info (helper <*> parseProxyConfig)
       <$> backendParser
       <*> strOption (long "db" <> metavar "STR" <> help "database connection string")
       <*> optional (strOption (long "drop-account" <> metavar "FILE" <> help "file with GTU drop account credentials (only used for stagenet and testnet)."))
+      <*> optional (strOption (long "forced-update-config" <> metavar "FILE" <> help "file with GTU drop account credentials (only used for stagenet and testnet)."))
       <*> optional (option auto (long "health-tolerance" <> metavar "SECONDS" <> help "the maximum tolerated age of the last final block in seconds before the health query returns false."))
       <*> strOption (long "ip-data" <> metavar "FILE" <> help "File with public and private information on the identity providers, together with metadata.")
     mkProxyConfig backend = ProxyConfig $ GrpcConfig
@@ -72,6 +75,32 @@ accountParser = AE.withObject "Account keys" $ \v -> do
             return $ take (fromIntegral keyThreshold) $ Map.toAscList ks
           return (accountAddr, take (fromIntegral threshold) (Map.toAscList keysMap))
 
+forcedUpdateConfigParser :: AE.Value -> AE.Parser (Maybe ForcedUpdateConfig)
+forcedUpdateConfigParser = AE.withObject "Forced update config" $ \v -> do
+  forceString <- v AE..:? "forceUpdateVersions" AE..!= ""
+  suggestString <- v AE..:? "suggestUpdateVersions" AE..!= ""
+  mfucURL <- v AE..:? "url"
+  case parseRanges forceString of
+    Left err -> fail $ "Invalid forced update range: " ++ show err
+    Right fucForceUpdate -> case parseRanges suggestString of
+      Left err -> fail $ "Invalid suggest update range: " ++ show err
+      Right fucSuggestUpdate ->
+        case mfucURL of
+          Just fucURL -> return $ Just ForcedUpdateConfig{..}
+          Nothing | null fucForceUpdate && null fucSuggestUpdate -> return Nothing
+                  | otherwise -> fail "If non-empty ranges are given then the URL must be present."
+
+-- |Return a pair of configurations, the first one for iOS the second for Android.
+forcedUpdateParser :: AE.Value -> AE.Parser (Maybe ForcedUpdateConfig, Maybe ForcedUpdateConfig)
+forcedUpdateParser = AE.withObject "Forced update configs" $ \v -> do
+  ios <- v AE..:? "ios" >>= \case
+    Nothing -> return Nothing
+    Just vv -> forcedUpdateConfigParser vv
+  android <- v AE..:? "android" >>= \case
+    Nothing -> return Nothing
+    Just vv -> forcedUpdateConfigParser vv
+  return (ios, android)
+
 main :: IO ()
 main = do
   ProxyConfig{..} <- execParser parser
@@ -85,6 +114,14 @@ main = do
       case getKeys of
         Left err -> die $ "Cannot parse account keys: " ++ show err
         Right (dropAccount, dropKeys) -> return . Just $ GTUDropData {..}
+  (forcedUpdateConfigIOS, forcedUpdateConfigAndroid) <- case pcForcedUpdateConfigFile of
+    Nothing -> return (Nothing, Nothing)
+    Just fuFileName -> do
+      fuFile <- LBS.readFile fuFileName
+      let getKeys = AE.eitherDecode' fuFile >>= AE.parseEither forcedUpdateParser
+      case getKeys of
+        Left err -> die $ "Cannot parse forced update config: " ++ show err
+        Right cfg -> return cfg
   Right ipInfo <- AE.eitherDecode' <$> LBS.readFile pcIpInfo
   runStderrLoggingT $ withPostgresqlPool pcDBConnString 10 $ \dbConnectionPool -> liftIO $ do
     -- do not care about the gtu receipients database if gtu drop is not enabled
