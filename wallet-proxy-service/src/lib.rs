e9a4dcd3ef443553e1ab248d86874bae5f3f73b7
Allan Rasmussen <allanbrondum@gmail.com>
2025-09-09 09:47:49 +0200

    fix

--- wallet-proxy-service/src/lib.rs
+++ wallet-proxy-service/src/lib.rs
@@ -1,5 +1,6 @@
 /// Configuration of the Wallet Proxy service
 pub mod configuration;
+/// Prometheus metrics registry
 mod metrics_registry;
 /// Monitoring API router (health, metrics, etc.)
 mod monitoring_api;
