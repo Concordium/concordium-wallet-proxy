e1bdc401717fb3ebfbea29341cb612e3b1882add
Allan Rasmussen <allanbrondum@gmail.com>
2025-09-09 09:47:14 +0200

    fix

--- wallet-proxy-service/src/lib.rs
+++ wallet-proxy-service/src/lib.rs
@@ -2,6 +2,6 @@
 pub mod configuration;
 mod metrics_registry;
 /// Monitoring API router (health, metrics, etc.)
-pub mod monitoring_api;
+mod monitoring_api;
 /// The Wallet Proxy service, including all endpoints
 pub mod service;
--- wallet-proxy-service/src/metrics_registry.rs
+++ wallet-proxy-service/src/metrics_registry.rs
@@ -2,4 +2,5 @@ use parking_lot::Mutex;
 use prometheus_client::registry::Registry;
 use std::sync::LazyLock;
 
+/// The prometheus metrics registry for the service
 pub static REGISTRY: LazyLock<Mutex<Registry>> = LazyLock::new(|| Mutex::new(Registry::default()));
